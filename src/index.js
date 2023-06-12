146c83863740eb1621fb5117ddb501ef592dbbd8
damgiacuong2102 <damgiacuong2102@gmail.com>
2023-06-12 23:27:38 +0700

    Update Code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const axios = require("axios")
+const app = express()
+const path = require('path');
 
-const url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0'
-fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-
-    });
-function handleData(data) {
-    console.log(data)
-}
+const url = 'https://pokeapi.co/api/v2/pokemon'
+app.get('/', async (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, '..', 'views', 'index.html'), (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    } catch (err) {
+        console.log(err);
+    }
+});
+app.listen(3000, () => {
+    console.log('sever is running on port 3000')
+})
